3aa2bb89337b9169d3d54c2e87f06ed656da2a29
Fireco82 <147770655+Fireco82@users.noreply.github.com>
2023-11-07 21:36:30 -0500

    Add files via upload

---
+++ wordcounter.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class wordcounter {
+    public static void main(String[] args) {
+        try {
+           
+            Map<String, Integer> wordCounts = new HashMap<>();
+
+            
+                BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+{}
+           
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Split the line into words based on spaces
+                     String[] words = line.toLowerCase().split(" ");
+
+                
+                for (String word : words) {
+                    if (!word.isEmpty()) {
+                        wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+
+            
+            reader.close();
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+
+           
+            for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+                writer.write(entry.getKey() + " " + entry.getValue());
+                writer.newLine();
+            }
+
+           
+            writer.close();
+
+            
+            
+                System.out.println("number of word to the  output.txt");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
